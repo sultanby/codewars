6a6ab91247707c8d98d5df1ba4a2a3df10713127
sultanby <jemjasott@gmail.com>
2020-12-11 22:57:01 +0100

    last kata

---
+++ Sum_of_all_the_multiples.js
@@ -0,0 +1,23 @@
+/* 
+Your task is to write function findSum.
+
+Upto and including n, this function will return the sum of all multiples of 3 and 5.
+
+For example:
+
+findSum(5) should return 8 (3 + 5)
+
+findSum(10) should return 33 (3 + 5 + 6 + 9 + 10)
+*/
+
+function findSum(n) {
+  var sum = 0;
+  var i;
+  for (i = 0; i <= n; i++) { 
+    if ((i % 3 == 0) || (i % 5 == 0)){
+      sum = sum + i;
+    }
+    continue;
+  }
+  return sum;
+}
